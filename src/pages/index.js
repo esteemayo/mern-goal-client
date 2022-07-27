22ba8c73bf18d678d6868f1bcff346fa272971d5
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-28 00:50:39 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard';
+import Login from './Login';
+import Register from './Register';
+import SharedLayout from './SharedLayout';
+
+export { Dashboard, Login, Register, SharedLayout };
