3332458411eedbe833e6884b35a89e887eb368d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-29 00:48:45 +0100

    added user authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 
+import { reset, setLogout } from 'features/auth/authSlice';
+
 const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state.auth }));
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+    dispatch(reset());
+    navigate('/');
+  };
+
   return (
     <header className='header'>
       <div className='logo'>
         <Link to='/'>Goalsetter</Link>
       </div>
       <ul>
-        <li>
-          <Link to='/login'>
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to='/register'>
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button className='btn' onClick={handleLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
