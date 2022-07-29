1f2a3ef7fe70e0bd276c95fa77506a52293fd406
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-29 03:50:49 +0100

    handled user registration and file upload

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,18 @@
-import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+
+import app from '../firebase';
+import Spinner from 'components/Spinner';
+import { registerUser, reset } from 'features/auth/authSlice';
 
 const initialState = {
   name: '',
@@ -9,6 +22,15 @@ const initialState = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => ({ ...state.auth })
+  );
+
+  const nameRef = useRef();
+  const [per, setPer] = useState(null);
+  const [file, setFile] = useState(null);
   const [formData, setFormData] = useState(initialState);
 
   const handleChange = ({ target: input }) => {
@@ -16,9 +38,73 @@ const Register = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+  const { password, confirmPassword } = formData;
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const fileName = `${new Date().getTime()}-${file.name}`;
+
+      const storage = getStorage(app);
+      const storageRef = ref(storage, `users/${fileName}`);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          setPer(progress);
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setFormData((prev) => ({ ...prev, avatar: downloadURL }));
+          });
+        }
+      );
+    };
+
+    file && uploadFile();
+  }, [file]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      return toast.error('Passwords do not match');
+    }
+
+    const userData = {
+      ...formData,
+    };
+
+    dispatch(registerUser({ userData, toast }));
   };
+
+  useEffect(() => {
+    nameRef.current.focus();
+    isError && toast.error(message);
+    isSuccess && user && navigate('/');
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -33,11 +119,21 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
           <div className='form-group'>
             <input
+              type='file'
+              name='file'
+              id='file'
+              className='form-control'
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </div>
+          <div className='form-group'>
+            <input
               type='text'
               name='name'
               id='name'
               placeholder='Enter your name'
               className='form-control'
+              ref={nameRef}
               onChange={handleChange}
             />
           </div>
@@ -72,7 +168,11 @@ const Register = () => {
             />
           </div>
           <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
+            <button
+              type='submit'
+              className='btn btn-block'
+              disabled={per !== null && per < 100}
+            >
               Submit
             </button>
           </div>
