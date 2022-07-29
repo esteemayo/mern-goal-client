7b547c8b27d2f74f255bde2f589af603e0966ab1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-29 02:00:18 +0100

    implemented login authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,47 @@
-import { useRef } from 'react';
+import { toast } from 'react-toastify';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Spinner from 'components/Spinner';
+import { loginUser, reset } from 'features/auth/authSlice';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => ({ ...state.auth })
+  );
+
   const emailRef = useRef();
-  const passwordRef = useRef();
+  const [formData, setFormData] = useState(null);
+
+  const handleChange = ({ target: input }) => {
+    const { name, value } = input;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      ...formData,
+    };
+
+    dispatch(loginUser({ userData, toast }));
   };
+
+  useEffect(() => {
+    emailRef.current.focus();
+    isError && toast.error(message);
+    isSuccess && user && navigate('/');
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -22,22 +56,24 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <div className='form-group'>
             <input
+              id='email'
               type='email'
               name='email'
-              id='email'
               placeholder='Enter your email'
               className='form-control'
               ref={emailRef}
+              onChange={handleChange}
             />
           </div>
           <div className='form-group'>
             <input
+              id='password'
               type='password'
               name='password'
-              id='password'
               placeholder='Enter password'
               className='form-control'
-              ref={passwordRef}
+              onChange={handleChange}
+              minLength={8}
             />
           </div>
           <div className='form-group'>
