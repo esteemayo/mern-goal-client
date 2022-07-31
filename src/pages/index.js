aed76ebfacc75cd58c38c34b082b6d62377f3e22
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-31 01:14:57 +0100

    imported forgot and reset password pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,17 @@
 import Dashboard from './Dashboard';
+import ForgotPassword from './ForgotPassword';
 import Login from './Login';
+import NotFound from './NotFound';
 import Register from './Register';
+import ResetPassword from './ResetPassword';
 import SharedLayout from './SharedLayout';
 
-export { Dashboard, Login, Register, SharedLayout };
+export {
+  Dashboard,
+  ForgotPassword,
+  Login,
+  NotFound,
+  Register,
+  ResetPassword,
+  SharedLayout,
+};
