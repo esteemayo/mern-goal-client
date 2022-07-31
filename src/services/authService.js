e364c61332db9191fcf496038567197146a7e804
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-31 01:15:29 +0100

    added new endpoints

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,4 +6,10 @@ const tokenKey = 'accessToken';
 export const login = (credentials) =>
   http.post(`${apiEndpoint}/login`, credentials);
 
+export const forgot = (email) =>
+  http.post(`${apiEndpoint}/forgot-password`, email);
+
+export const reset = (token, credentials) =>
+  http.post(`${apiEndpoint}/reset-password/${token}`, credentials);
+
 export const getJwt = () => JSON.parse(localStorage.getItem(tokenKey))?.token;
