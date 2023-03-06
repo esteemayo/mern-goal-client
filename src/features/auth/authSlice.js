6eb9b4a66c1149539f69bf6c77e38ffd57cd13c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-06 15:38:53 +0100

    fix: modify

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,7 +2,6 @@ import jwtDecode from 'jwt-decode';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import * as authAPI from 'services/authService';
-import { getFromStorage, setToStorage, tokenKey } from 'utils';
 import { register } from 'services/userService';
 
 export const registerUser = createAsyncThunk(
