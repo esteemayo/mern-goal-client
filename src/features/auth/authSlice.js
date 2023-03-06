161992a1a8748a06e48fbd4002be3384b46dd12e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-06 08:11:01 +0100

    feat: import getFromStorage

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -3,6 +3,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import * as authAPI from 'services/authService';
 import { register } from 'services/userService';
+import { getFromStorage } from 'utils';
 
 export const registerUser = createAsyncThunk(
   'user/register',
