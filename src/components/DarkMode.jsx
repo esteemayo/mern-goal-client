b3f6cb42b4b9060824f852092d6b41bf5edf14b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-08 00:16:44 +0100

    feat: import react icons

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { BiSun } from 'react-icons/bi';
 import { toggle } from 'features/darkMode/darkModeSlice';
 
 const DarkMode = () => {
