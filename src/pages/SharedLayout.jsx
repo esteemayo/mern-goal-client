a85106dd3bf0367d0d78d055bdde4b536b4814cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:01:41 +0100

    fix: modify

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import Navbar from 'components/Navbar';
+import DarkMode from 'components/DarkMode';
 import 'react-toastify/dist/ReactToastify.css';
 
 const SharedLayout = () => {
