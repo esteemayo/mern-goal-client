84fa800113750e938d9e920793b27a68a8efea13
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:22:15 +0100

    fix: modify

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import authReducer from '../features/auth/authSlice';
+import authReducer from 'features/auth/authSlice';
 import goalReducer from 'features/goals/goalSlice';
 import darkModeReducer from 'features/darkMode/darkModeSlice';
 
