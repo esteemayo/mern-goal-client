7f589a55193d9e881556da4d73c6525a9cac3749
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:29:45 +0100

    feat: import useSelector react redux

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggle } from 'features/darkMode/darkModeSlice';
 
 const DarkMode = () => {
