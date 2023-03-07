36b7920aef9867d0562a6167b0fb22d40fb9e658
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:20:49 +0100

    feat: import darkMode slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../features/auth/authSlice';
 import goalReducer from '../features/goals/goalSlice';
+import darkModeReducer from 'features/darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
