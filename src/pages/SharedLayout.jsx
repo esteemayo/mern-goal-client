ffc55b9777380de82951ee128728b9859e5db6e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:01:03 +0100

    feat: import DarkMode component

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 
 import Navbar from 'components/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
+import DarkMode from 'components/DarkMode';
 
 const SharedLayout = () => {
   return (
