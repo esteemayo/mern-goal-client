a2c4cc41609ac9830029c7c164f55a3a7736309a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 01:26:49 +0100

    fix: modify

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,6 +1,8 @@
 import { useDispatch } from 'react-redux';
 
 const DarkMode = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className='container-darkmode'>
       <div className='wrapper'>dark</div>
