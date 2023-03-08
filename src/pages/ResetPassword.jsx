6c8975dbff8b015078b10cc0db987d341744c160
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-08 02:18:27 +0100

    fix: modify

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { FaUserCog } from 'react-icons/fa';
-import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 
