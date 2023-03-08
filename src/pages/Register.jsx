a6b2a02484fc82e340d4479a556df530e2a7e6be
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-08 01:22:54 +0100

    fix: modify

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,6 @@ import {
 import Spinner from 'components/Spinner';
 import app from '../firebase';
 import FormButton from 'components/FormButton';
-import FormInput from 'components/FormInput';
 import { registerUser, reset } from 'features/auth/authSlice';
 
 const initialState = {
