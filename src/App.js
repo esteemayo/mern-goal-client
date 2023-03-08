c0f2ff383305c33922984313ad4a9c30102b3fb3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-08 02:24:54 +0100

    fix: rename props name

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
                 path='register'
                 element={
                   <ProtectedRoute>
-                    <Register registerInputs={registerInputs} />
+                    <Register inputs={registerInputs} />
                   </ProtectedRoute>
                 }
               />
