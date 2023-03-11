8cfd34dd58aac93de1b09b1602cd70fe3c87092c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 01:25:12 +0100

    feat: add update goal api endpoint

--- src/services/goalService.js
+++ src/services/goalService.js
@@ -8,4 +8,6 @@ export const getGoals = () => http.get(apiEndpoint);
 
 export const createGoal = (goalData) => http.post(apiEndpoint, goalData);
 
+export const updateGoal = (goalId, newGoal) => http.patch(goalUrl(goalId), newGoal);
+
 export const deleteGoal = (goalId) => http.delete(goalUrl(goalId));
