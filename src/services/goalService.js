3f785799d7e9565a272f96173711c24ca7b28398
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 01:25:29 +0100

    fix: format code

--- src/services/goalService.js
+++ src/services/goalService.js
@@ -8,6 +8,7 @@ export const getGoals = () => http.get(apiEndpoint);
 
 export const createGoal = (goalData) => http.post(apiEndpoint, goalData);
 
-export const updateGoal = (goalId, newGoal) => http.patch(goalUrl(goalId), newGoal);
+export const updateGoal = (goalId, newGoal) =>
+  http.patch(goalUrl(goalId), newGoal);
 
 export const deleteGoal = (goalId) => http.delete(goalUrl(goalId));
