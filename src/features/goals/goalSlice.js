dbf5d13e3dc346b5de1907b058253eb90aeeb794
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-11 02:08:22 +0100

    fix: modify

--- src/features/goals/goalSlice.js
+++ src/features/goals/goalSlice.js
@@ -93,21 +93,6 @@ const goalSlice = createSlice({
       state.isError = true;
       state.message = payload.message;
     },
-    [editGoal.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [editGoal.fulfilled]: (state, { payload }) => {
-      state.isLoading = false;
-      state.isSuccess = true;
-      state.goals[
-        state.goals.findIndex((item) => item._id === payload._id)
-      ] = payload;
-    },
-    [editGoal.rejected]: (state, { payload }) => {
-      state.isLoading = false;
-      state.isError = true;
-      state.message = payload.message;
-    },
     [removeGoal.pending]: (state) => {
       state.isLoading = true;
     },
